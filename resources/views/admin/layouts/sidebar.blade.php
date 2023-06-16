74f1d7cbd0a4fc77c98efb6b193c2358409d721f
Vincent-Kamotho <vinsentwambugu@gmail.com>
2023-06-17 00:42:41 +0300

    This is an admin reference template

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,134 @@
+<div class="vertical-menu">
+
+    <div data-simplebar class="h-100">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+
+                <li>
+                    <a href="#" class="waves-effect">
+                        <i class="bx bx-home-circle"></i>
+                        <span key="t-dashboards">Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="has-arrow waves-effect">
+                        <i class="bx bx-layout"></i>
+                        <span key="t-layouts">Layouts</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="true">
+                        <li>
+                            <a href="#"  key="t-vertical">Vertical</a>
+                        </li>
+
+                        <li>
+                            <a href="#"  key="t-horizontal">Horizontal</a>
+                            
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow">
+                        <i class="bx bx-calendar"></i>
+                        <span key="t-dashboards">Calendars</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#" key="t-tui-calendar">TUI Calendar</a></li>
+                        <li><a href="#" key="t-full-calendar">Full Calendar</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="#" class="waves-effect">
+                        <i class="bx bx-chat"></i>
+                        <span key="t-chat">Chat</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="waves-effect">
+                        <i class="bx bx-file"></i>
+                        
+                        <span key="t-file-manager">File Manager</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="has-arrow waves-effect">
+                        <i class="bx bx-store"></i>
+                        <span key="t-ecommerce">Ecommerce</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#" key="t-products">Products</a></li>
+                        <li><a href="#" key="t-product-detail">Product Detail</a></li>
+                        <li><a href="#" key="t-orders">Orders</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="#" class="has-arrow waves-effect">
+                        <i class="bx bx-bitcoin"></i>
+                        <span key="t-crypto">Crypto</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#" key="t-wallet">Wallet</a></li>
+                        <li><a href="#" key="t-buy">Buy/Sell</a></li>
+                        <li><a href="#" key="t-exchange">Exchange</a></li>
+                        
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="#" class="has-arrow waves-effect">
+                        <i class="bx bx-receipt"></i>
+                        <span key="t-invoices">Invoices</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#" key="t-invoice-list">Invoice List</a></li>
+                        <li><a href="#" key="t-invoice-detail">Invoice Detail</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="#" class="has-arrow waves-effect">
+                        <i class="bx bx-briefcase-alt-2"></i>
+                        <span key="t-projects">Projects</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#" key="t-p-grid">Projects Grid</a></li>
+                        <li><a href="#" key="t-p-list">Projects List</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="#" class="has-arrow waves-effect">
+                        <i class="bx bx-task"></i>
+                        <span key="t-tasks">Tasks</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="#" key="t-task-list">Task List</a></li>
+                        <li><a href="#" key="t-kanban-board">Kanban Board</a></li>
+                        <li><a href="#" key="t-create-task">Create Task</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bxs-user-detail"></i>
+                        <span key="t-contacts">Contacts</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="contacts-grid.html" key="t-user-grid">Users Grid</a></li>
+                        <li><a href="contacts-list.html" key="t-user-list">Users List</a></li>
+                        <li><a href="contacts-profile.html" key="t-profile">Profile</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
